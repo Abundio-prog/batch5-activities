9a6aac98ba38d3d21659dc4e15a0fa1cf63acb91
Abundio-prog <73151278+Abundio-prog@users.noreply.github.com>
2020-12-18 13:09:44 +0700

    New Calc

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,111 +1,40 @@
-const calculator = {
-      displayValue: '0',
-      firstOperand: null,
-      waitingForSecondOperand: false,
-      operator: null,
-    };
-    
-    function inputDigit(digit) {
-      const { displayValue, waitingForSecondOperand } = calculator;
-    
-      if (waitingForSecondOperand === true) {
-        calculator.displayValue = digit;
-        calculator.waitingForSecondOperand = false;
-      } else {
-        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-      }
-    }
-    
-    function inputDecimal(dot) {
-      if (calculator.waitingForSecondOperand === true) {
-            calculator.displayValue = "0."
-        calculator.waitingForSecondOperand = false;
-        return
-      }
-    
-      if (!calculator.displayValue.includes(dot)) {
-        calculator.displayValue += dot;
-      }
-    }
-    
-    function handleOperator(nextOperator) {
-      const { firstOperand, displayValue, operator } = calculator
-      const inputValue = parseFloat(displayValue);
+//document.querySelector('.display').value = document.querySelector('#numbers').innerHTML
+const display = document.getElementById('display');
+const num  = document.querySelectorAll('.numbers button');
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear');
+const operator = document.querySelectorAll('operator button');
+
+const valInpt = false;
+
+equals.addEventListener('click', ()=>{
+   display.value  = eval(display.value);
+});
+
+clear.addEventListener('click', ()=>{
+   display.value  = "";
+});
+
+
+
+for(let i = 0;i<=num.length; i++){
+   
+   num[i].addEventListener('click', (e) =>{
       
-      if (operator && calculator.waitingForSecondOperand)  {
-        calculator.operator = nextOperator;
-        return;
-      }
-    
-    
-      if (firstOperand == null && !isNaN(inputValue)) {
-        calculator.firstOperand = inputValue;
-      } else if (operator) {
-        const result = calculate(firstOperand, inputValue, operator);
-    
-        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-        calculator.firstOperand = result;
-      }
-    
-      calculator.waitingForSecondOperand = true;
-      calculator.operator = nextOperator;
-    }
-    
-    function calculate(firstOperand, secondOperand, operator) {
-      if (operator === '+') {
-        return firstOperand + secondOperand;
-      } else if (operator === '-') {
-        return firstOperand - secondOperand;
-      } else if (operator === '*') {
-        return firstOperand * secondOperand;
-      } else if (operator === '/') {
-        return firstOperand / secondOperand;
-      }
-    
-      return secondOperand;
-    }
-    
-    function resetCalculator() {
-      calculator.displayValue = '0';
-      calculator.firstOperand = null;
-      calculator.waitingForSecondOperand = false;
-      calculator.operator = null;
-    }
-    
-    function updateDisplay() {
-      const display = document.querySelector('.calculator-screen');
-      display.value = calculator.displayValue;
-    }
-    
-    updateDisplay();
-    
-    const keys = document.querySelector('.calculator-keys');
-    keys.addEventListener('click', event => {
-      const { target } = event;
-      const { value } = target;
-      if (!target.matches('button')) {
-        return;
-      }
-    
-      switch (value) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '=':
-          handleOperator(value);
-          break;
-        case '.':
-          inputDecimal(value);
-          break;
-        case 'all-clear':
-          resetCalculator();
-          break;
-        default:
-          if (Number.isInteger(parseFloat(value))) {
-            inputDigit(value);
-          }
-      }
-    
-      updateDisplay();
-    });
+       const newInpt = display.value;
+       const addChar = newInpt[newInpt.length -1];
+
+       if (valInpt === false) {
+           display.value += e.target.innerHTML;
+         } 
+       else if(valInpt === true && addChar === "+"|| addChar === "-"|| addChar === "*" ||addChar === "/"){
+           valInpt = false;
+           display.value = "";
+           display.value += e.target.innerHTML;
+
+       }
+
+   });
+   
+}
+
